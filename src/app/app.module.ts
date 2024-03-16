f61a866c89f51b8b0435301f94ec9c00f44cad93
PHAN DUC <ducpa2002@gmail.com>
2024-03-16 22:32:31 +0700

    update login, register, order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { OrderComponent } from './order/order.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 
 @NgModule({
   declarations: [    
-    HomeComponent, HeaderComponent, FooterComponent
+    HomeComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    OrderComponent, 
+    DetailProductComponent,
+    LoginComponent, 
+    RegisterComponent, 
+    OrderConfirmComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [
-    HomeComponent
+    // HomeComponent
+    DetailProductComponent
+    // OrderComponent
+    // LoginComponent
+    // RegisterComponent
+    // OrderConfirmComponent
   ]
 })
 export class AppModule { }
